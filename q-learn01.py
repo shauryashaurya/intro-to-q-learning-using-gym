8e6a9b813252bad4c7d0fe44ee9cf2a5562b24c5
Shaurya <shauryashaurya@gmail.com>
2019-10-08 10:09:16 +0530

    Update q-learn01.py

--- q-learn01.py
+++ q-learn01.py
@@ -4,12 +4,17 @@ import numpy as np
 env = gym.make("MountainCar-v0")
 env.reset()
 
+# while this env provides a min/max - 
+# IRL we may need to run observations for min/max may not be available at the start
+
 # print the max position and max velocity possible
 print("env.observation_space.high: [position, velocity]: ", env.observation_space.high)
 # print the min position and min velocity possible
 print("env.observation_space.low:  [position, velocity]: ", env.observation_space.low)
 
+# break the position range into 20 buckets and the velocity range into 20 as well
 DISCREET_OS_SIZE = [20,20]
+# size of one bucket of [position, velocity]
 discreet_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCREET_OS_SIZE
 
 q_table = np.random.uniform(low = -2, high = 0, size = (DISCREET_OS_SIZE + [env.action_space.n]))
